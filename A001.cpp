21006cec219cb03bb744af87098827f07ea0a5ec
Wayne-Yang-Crosshair <125975079+Wayne-Yang-Crosshair@users.noreply.github.com>
2024-02-03 00:00:59 +0800

    Add files via upload

---
+++ A001.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#include<iostream>
+
+using namespace std;
+int test=1;
+int main(void){
+    while(test){
+        int a=0,b=0,c=0,d=0;
+        cin>>a>>b>>c>>d;
+        cout<<a*b*c*d;
+    }
+    return 0;
+}
+
